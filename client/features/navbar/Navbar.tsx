dea4262ee4fabcb3c0c9116d165f3ba22d8d0bd8
Rachel Eckert <rachel.eckert13@gmail.com>
2023-05-02 16:29:44 -0400

    state not updating on navbar link

--- client/features/navbar/Navbar.tsx
+++ client/features/navbar/Navbar.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { Typography, AppBar, Box, Toolbar } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>FS-App-Template</h1>
-      <nav>
-        <div>
-          <Link to="/home">Home</Link>
-        </div>
-      </nav>
-      <hr />
-    </div>
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <Typography component={Link} to="/home" sx={{ color: "black" }}>
+            Home
+          </Typography>
+
+          <Typography component={Link} to="/family/1" sx={{ color: "black" }}>
+            Alligatoroidea
+          </Typography>
+
+          <Typography component={Link} to="/family/2" sx={{ color: "black" }}>
+            Crocodylidae
+          </Typography>
+
+          <Typography component={Link} to="/family/3" sx={{ color: "black" }}>
+            Gavialidae
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
